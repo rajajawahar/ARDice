b68ec4671621f12c51c615a8e2fe2156f0195732
Raja Jawahar <rajajawahar77@gmail.com>
2018-09-03 19:25:38 +0530

    3D Object created

--- ARDice/ViewController.swift
+++ ARDice/ViewController.swift
@@ -23,6 +23,19 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
+        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.red
+        cube.materials = [material]
+        
+        
+        let node = SCNNode()
+        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
+        node.geometry = cube
+        sceneView.scene.rootNode.addChildNode(node)
+        
+        
+        
 //        // Create a new scene
 //        let scene = SCNScene(named: "art.scnassets/ship.scn")!
 //
